f413add4351d0eb24fa4d2cd1821b60fee50e9aa
unknown <mahbub.progressivebyte@gmail.com>
2025-12-22 13:59:49 +0600

    misc

--- inc/class-custom-field.php
+++ inc/class-custom-field.php
@@ -27,8 +27,8 @@ if (! class_exists('Multi_Location_Custom_Field')):
         {
             add_filter('atbdp_form_preset_widgets', [$this, 'register_custom_field']);
             add_filter('atbdp_single_listing_content_widgets', [$this, 'single_listing_content_widgets']);
-            // add_filter('directorist_field_template', [$this, 'directorist_field_template'], 10, 2);
-            add_filter('directorist_single_item_template', [$this, 'directorist_single_item_template'], 10, 2);
+            add_filter('directorist_field_template', [$this, 'directorist_field_template'], 10, 2);
+            // add_filter('directorist_single_item_template', [$this, 'directorist_single_item_template'], 10, 2);
 
             add_filter( 'directorist_template', [ $this, 'change_template' ], 20, 2 );            
         }
